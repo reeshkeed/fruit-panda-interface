998bfb651ae83aee5df4a19750450fc02e1035fd
reeshkeed <hello@girishdaloso.com>
2023-01-17 19:00:03 +0800

    Created navigation bar

---
+++ src/components/layout/NavbarMenu.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <header
+    class="navbar bg-dark-500 flex justify-between items-center p-3 w-auto"
+  >
+    <img alt="Girish Daloso" class="h-16 w-16" src="@/assets/logo.svg" />
+
+    <div class="">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style></style>
